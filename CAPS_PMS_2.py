7266d2d21efbaadf6d0a6774266457e0e83ec464
Abhisekh-Mundle-99 <58269110+Abhisekh-Mundle-99@users.noreply.github.com>
2020-06-28 01:08:54 +0530

    Part2
    Added Files

---
+++ CAPS_PMS_2.py
@@ -0,0 +1,253 @@
+import tkinter as tk
+import tkinter.messagebox as msg
+import DBMS
+
+
+# Main Application Class
+class Application(tk.Tk):
+    # app defaults
+    min_height = 640
+    min_width = 520
+    cur_height = 640
+    cur_width = 520
+    max_height = 1024
+    max_width = 1024
+
+    # fonts
+    c_tribars = '\u2261'
+    c_search = '\uD836'  # '\uD83D' '\uDD0D'
+    c_sun = '\u2600'
+    c_moon = '\u263E'  # '\uD83C' '\uDF18'
+    c_rupay = '₹'
+
+    menu_font = 'Century Gothic'
+    menu_fontsize = 30
+
+    # color schemes
+    dark = [{"bg": "black", "fg": "white"}, {"bg": "darkgrey", "fg": "white"}]
+    light = [{"bg": "ivory", "fg": "black"}, {"bg": "seablue", "fg": "grey"}]
+
+    # ui options | menu names
+    app_name = "CAPS PMS v1.0"
+    app_icon = "icon.ico"
+
+    # tooltips | descriptions
+
+    # flags for app states:
+    #   full_screen: false, default,    app doesnt start with full screen
+    #   dark_mode:  true, default,      color scheme is set to dark by default
+    #   message_open: false, default    toggled to check if a message box is already active
+    #   logged_in: false, default,      toggled to check if user has logged in, when already
+    #                                   logged in, only password confirmation is required for
+    #                                   other things
+    is_fullscreen = False
+    is_dark = True
+    msg_open = False
+    is_logged_in = True
+
+    # control variables
+    cursor_x = 0
+    cursor_y = 0
+
+    # default initialization function called on class instantiation
+    def __init__(self):
+        super().__init__()
+
+        self.title(self.app_name)
+        self.iconbitmap(self.app_icon)
+        self.setup_look_n_feel()
+
+    # initialize application with default UI preferences
+    def setup_look_n_feel(self):
+        if self.is_fullscreen:
+            self.state('zoomed')
+        else:
+            self.geometry("{0}x{1}+0+0".format(self.min_height, self.min_width))
+
+        # store screen dimensions for later use
+        self.max_width = self.winfo_screenwidth()
+        self.max_height = self.winfo_screenheight()
+
+        # ready color scheme
+        if self.is_dark:
+            bg_color = self.dark[0].get('bg')
+            fg_color = self.dark[0].get('fg')
+        elif self.is_light:
+            bg_color = self.light[0].get('bg')
+            fg_color = self.light[0].get('fg')
+        else:
+            bg_color = self.custom[0].get('bg')
+            fg_color = self.custom[0].get('fg')
+
+        # define all top menus
+        self.main_menu = tk.Menu(self, bg=bg_color, fg=fg_color, borderwidth=0, tearoff=0)
+        self.main_menu.config(font=(self.menu_font, self.menu_fontsize))
+
+        # add menu commands or cascades or submenus in desired order
+        # cascades are menu categories like File, Edit, View, etc
+        # and insert sub menus or commands in them
+        self.home_menu = tk.Menu(self, bg=bg_color, fg=fg_color, borderwidth=0, tearoff=0)
+        self.login_btn = self.main_menu.add_cascade(label=self.c_tribars, menu=self.home_menu, font=(self.menu_font, 100), command=self.create_login())
+        self.menu_btn = self.main_menu.add_cascade(label=self.c_tribars, menu=self.home_menu, font=(self.menu_font, 100))
+        self.search_btn = self.main_menu.add_command(label=self.c_search, command=self.popup_search)
+        self.theme_btn = self.main_menu.add_command(label=self.c_moon, command=switch_theme)
+        self.bill_btn = self.main_menu.add_command(label=self.c_rupay, command=self.popup_bill)
+
+        # complete configuration and formatting
+        self.config(menu=self.main_menu)
+
+        tk.Label(self, text="YOUR WORKSPACE", bg="cyan4", fg="white", font="Arial 25").pack()
+
+        # run repeatable behaviour for first time
+        self.update()
+
+        self.protocol('WM_DELETE_WINDOW', lambda: close_window(self,"Quit " + self.app_name + "?"))
+
+# POPUPS    
+# Search Menu
+    def popup_search(self):
+        DBMS.SearchForm(self)
+
+#login Form
+    def create_login(self):
+        DBMS.LoginForm(self)
+
+
+# Billing Form
+    def popup_bill(self):
+        DBMS.BillingForm(self)
+
+# Update Components On Resize
+    def update(self):
+        super(Application, self).update()
+        if self.is_fullscreen:
+            self.grid(self.cur_width * 0.8, self.cur_height * 0.8)
+            # self.main_menu.grid()
+
+# Setup Default Theme & Menus
+    def setup_theme(self):
+
+        # override default titlebar, geometry
+        # self.overrideredirect(True)
+        if self.is_fullscreen:
+            self.geometry("{0}x{1}+0+0".format(self.max_width, self.max_height))
+        else:
+            self.geometry("{0}x{1}+0+0".format(self.min_width, self.min_height))
+
+        # ready color scheme
+        if self.is_dark:
+            bg_color = self.dark[0].get('bg')
+            fg_color = self.dark[0].get('fg')
+        elif self.is_light:
+            bg_color = self.light[0].get('bg')
+            fg_color = self.light[0].get('fg')
+        else:
+            bg_color = self.custom[0].get('bg')
+            fg_color = self.custom[0].get('fg')
+
+    # Generate GUI Elements
+        # title bar
+        title_bar = tk.Frame(self, bg=bg_color, relief='raised', bd=2)
+        title_bar.geometry('{0}x{1}', 100, self.winfo_height())
+
+        # top menu
+        self.menu = tk.Menu(self, bg=bg_color, fg=fg_color)
+
+        # right side buttons
+        minimize_button = tk.Button(title_bar, text='_', command=self.set_minimize)
+        maximize_button = tk.Button(title_bar, text='+', command=self.set_fullscreen)
+        close_button = tk.Button(title_bar, text='x', command=lambda: self.close_window("Quit " + self.app_name + "?"))
+
+        # canvas
+        window = tk.Canvas(self, bg=bg_color)
+
+        # pack the widgets
+        title_bar.pack(expand=1, fill='x')
+        close_button.pack(side='right')
+        maximize_button.pack(side='right')
+        minimize_button.pack(side='right')
+        window.pack(expand=1, fill='both')
+
+        # bind title bar left-button click-hold and motion to window
+        title_bar.bind('<Button-1>', self.hold_window)
+
+
+# UI COMPONENTS & BEHAVIOUR
+
+# Toggle Theme
+def switch_theme(self):
+    self.is_dark = not self.is_dark
+    if self.is_dark:
+        self.theme_btn.config(label=self.c_moon)
+    else:
+        self.theme_btn.config(label=self.c_sun)
+
+
+# Close Button
+def close_window(self, message, event=None):
+    # this confirmation window operation can be generalized for more than purpose
+    if msg.askokcancel("Quit", message):
+        if event is None:
+            try:
+                self.destroy()
+            except:
+                pass
+        else:
+            try:
+                event.widget.destroy()
+            except:
+                pass
+
+
+# Confirm Certain Tasks, W/O Password
+def confirm_task(self, event=None, message="Please Confirm", req_pass=False):
+    # generalized form for a confirmation message
+    msg_box = msg.Message(self)
+    Label(msg_box, text=message)
+    if req_pass:
+        Label(msg_box, text="Password")
+        password = Entry(msg_box, width=20)
+        password.bind("<Return>", check)
+
+
+# Click-n-Hold Titlebar To Move Window
+def hold_window(self, event):
+    start_x = event.x_root
+    start_y = event.y_root
+
+    relative_x = app.winfo_x() - event.x_root
+    relative_y = app.winfo_y() - event.y_root
+
+    def move_window(_event):
+        self.geometry('+{0}+{1}'.format(_event.x_root + relative_x, _event.y_root + relative_y))
+
+    start_x = _event.x_root
+    start_y = _event.y_root
+
+    # bind title bar motion to window
+    self.bind('<B1-Motion>', move_window)
+
+
+# Minimize Button
+def set_minimize(self):
+    self.state('withdrawn')
+
+
+# Toggle Full Screen
+def set_fullscreen(self):
+    if self.is_fullscreen:
+        self.state('normal')
+        self.is_fullscreen = False
+    elif not self.is_fullscreen:
+        self.state('zoomed')
+        self.is_fullscreen = True
+
+    # updating state variables to re-place UI components
+    cur_height = self.winfo_screenheight()
+    cur_width = self.winfo_screenwidth()
+
+
+# Instantiating Application
+if __name__ == "__main__":
+    app = Application()
+    app.mainloop()
---
+++ DBMS.py
@@ -0,0 +1,366 @@
+import tkinter as tk
+import tkinter.messagebox as msg
+from datetime import date
+import time
+import sqlite3
+import tempfile
+import win32api
+import win32print
+
+"""Position a widget in the parent widget in a grid. Use as options:
+        column=number - use cell identified with given column (starting with 0)
+        columnspan=number - this widget will span several columns
+        in=master - use master to contain this widget
+        in_=master - see 'in' option description
+        ipadx=amount - add internal padding in x direction
+        ipady=amount - add internal padding in y direction
+        padx=amount - add padding in x direction
+        pady=amount - add padding in y direction
+        row=number - use cell identified with given row (starting with 0)
+        rowspan=number - this widget will span several rows
+        sticky=NSEW - if cell is larger on which sides will this
+                      widget stick to the cell boundary
+        """
+
+login_id = ''
+login_type = ''
+# two login types: admin | employee
+# employees cannot delete inventory
+admin_db = sqlite3.connect("admin.db")
+cur_log = admin_db.cursor()
+
+columns = ('Sl No', 'Name', 'Type', 'Quantity Left', 'Cost', 'Purpose', 'Expiry Date', 'Rack location', 'Manufacture')
+pharm_db = sqlite3.connect("medicine.db")
+cur_col = pharm_db.cursor()
+
+# bill processing globals
+cart = []
+names = []
+qty = []
+bill_id = 0
+
+
+""" STATIC UTILITY FUNCTIONS """
+
+
+# Get Current Date: Used to mark Bills, and check Expiry
+def current_date():
+    now = time.localtime()
+    cur_date = date(now[0], now[1], now[2])
+    return cur_date
+
+
+# Print temporary file to disk
+def print_file():
+    win32api.ShellExecute(0, "print", B, '/d:"%s"' % win32print.GetDefaultPrinter(), ".", 0)
+
+
+""" LOGIN FORM CLASS : REDIRECTS TO ADMIN OR EMPLOYEE """
+
+
+class LoginForm(tk.Toplevel):
+    def __init__(self, master, bg='white', fg='black'):
+        super().__init__()
+
+        self.master = master
+
+        self.title("Login")
+        self.geometry("300x150")
+
+        tk.Label(self, text="YOUR WORKSPACE", bg="cyan4", fg="white", font="Arial 25").pack(fill=tk.BOTH, expand=1)
+
+        tk.Label(self, text="Login ID").pack(fill=tk.BOTH, expand=1)
+        self.username = tk.Entry(self, bg=bg, fg=fg).pack(fill=tk.BOTH, expand=1)
+        tk.Label(self, text="Password").pack(fill=tk.BOTH, expand=1)
+        self.password = tk.Entry(self, bg=bg, fg=fg).pack(fill=tk.BOTH, expand=1)
+
+        self.submit_button = tk.Button(self, text="Login", command=self.submit, bg="pink").pack(fill=tk.X)
+        #self.submit_button = tk.Button(self, text="Demo", command=demo, bg="orange").pack(fill=tk.X)
+
+# Submit Button's function to collect uID, pass, perform checks, and highlight issues
+    def submit(self):
+        username = self.username.get()
+        password = self.password.get()
+
+        if not username:
+            self.username.configure(bg="red", fg="white")
+        elif not password:
+            self.password.configure(bg="red", fg="white")
+        elif username and password:
+            if not self.check():
+                self.username.configure(bg="red", fg="white")
+                self.password.configure(bg="red", fg="white")
+            else:
+                self.username.configure(bg="green", fg="white")
+                self.password.configure(bg="green", fg="white")
+                if login_type == "admin":
+                    self.master.admin_panel = AdminPanel(self.master)
+                    try:
+                        self.destroy()
+                    except:
+                        pass
+
+
+""" DATABASE LOOKUP FUNCTIONS """
+
+
+# MEDICINE & STOCK SEARCH FORM: EXPIRED STOCKS ARE UNLISTED FOR EMPLOYEES
+class SearchForm(tk.Toplevel):
+    def __init__(self, master, bg='white', fg='black'):
+        super().__init__()
+
+        self.master = master
+
+        self.title("Search Options")
+        self.geometry("300x150")
+
+        tk.Label(self, text="Search by - ").pack(fill=tk.BOTH, expand=1)
+        self.category_name = tk.Spinbox(self, bg=bg, fg=fg).pack(fill=tk.BOTH, expand=1)
+        self.category_value = tk.Entry(self, bg=bg, fg=fg).pack(fill=tk.BOTH, expand=1)
+        self.submit_button = tk.Button(self, text="Submit", command=self.submit).pack(fill=tk.X)
+        self.search_results = tk.Listbox(self, bg=fg, fg=bg).pack(fill=tk.BOTH, expand=1)
+
+# Submit Button's function to collect input for search & highlight problems
+    def submit(self):
+        category_id = self.category_name.get()
+        category_value = self.category_value.get()
+
+        if not category_id:
+            msg.showerror("Missing Information", "Please select search criteria!")
+        if not category_value:
+            msg.showerror("Missing Information", "Please enter a valid" + category_id
+                          + " to search!")
+        elif category_id and category_value:
+            if self.search():
+                tk.Button(self, label="[+]", command=lambda: append_to_cart(self), bg="blue", fg="white")
+                tk.Button(self, label="[-]", command=lambda: delete_from_cart(self), bg="red", fg="white")
+            else:
+                if category_id == "name":
+                    reason = "named "
+                elif category_id == "symptom":
+                    reason = "for "
+                else:
+                    reason = "with " + category_id + " "
+                msg.showinfo("Search Results", "We did not find any medicines " + reason + category_value)
+
+
+# User Login & Password Confirmation
+def check(self):
+    global login_id, login_type
+    if self.is_logged_in:
+        # If already logged in, this function checks password
+        # to confirm some regrettable actions like deleting inventory.
+        # The comment below stops IDE warning "login_id might not have any value"
+        # noinspection PyUnboundLocalVariable
+        cur_log.execute("select * from log where username='%s' and password='%s'"
+                        % (login_id, self.password.get()))
+        admin_db.commit()
+        # confirm password for logged-in admin
+        if cur_log.fetchone() is not None:
+            return True
+        else:
+            return False
+    else:
+        login_id = self.username.get()
+        cur_log.execute("select * from log where username='%s' and password='%s'"
+                        % (login_id, self.password.get()))
+        admin_db.commit()
+        if cur_log.fetchone() is not None:
+            return True
+        else:
+            login_type = cur_log.fetchone()[2]
+            return False
+
+
+# Begin Searching with provided category and value
+def search(self):
+    self.master.confirm_task(self, "Close Search?")
+    return True
+
+
+# Return Expiry Date of given stock
+def check_expiry(self, stock_num):
+    now = time.localtime()
+    cur_date = date(now[0], now[1], now[2])
+    return cur_date
+
+
+# Return True/False whether this stock has expired
+def check_expired(self, stock_num):
+    return current_date() > check_expiry(self, stock_num)
+
+
+""" DATABASE MODIFYING FUNCTIONS """
+
+
+# Delete Inventory: Only visible to Admin
+def dispose(self, med_name, stock_num=None):
+    if login_type != "admin":
+        self.msg.showerror("You are not authorized to delete inventory!")
+        return False
+    stocks_state = " "
+    confirm_disposal = "Confirm disposal?"
+    if stock_num:
+        if check_expired(stock_num):
+            stocks_state += "Stock no. " + str(stock_num) + " of " + med_name + " has expired!"
+        else:
+            stocks_state += "Stock no. " + str(stock_num) + " of " + med_name + " has NOT expired yet!"
+    else:
+        expired = count_expired_stocks(self, med_name)
+        stock_count = count_stocks(self, med_name)
+
+        stocks_state += expired + " out of " + stock_count + " stocks of " + med_name + " have expired."
+        confirm_disposal = " Dispose ALL or Expired stocks?"
+    if self.master.confirm_task(self, stocks_state + confirm_disposal):
+        return True
+    else:
+        return False
+
+
+# BILLING SYSTEM
+class BillingForm(tk.Toplevel):
+    def __init__(self, master, bg='white', fg='black'):
+        super().__init__()
+
+        self.master = master
+
+        self.title("Billing")
+        self.geometry("500x600+{0}+{1}".format(int(self.winfo_screenwidth()/2), int(self.winfo_screenheight())))
+
+        t = 0
+        names = []
+        qty = []
+        sl = []
+        n = []
+        qtys = [''] * 10
+
+        cur_col.execute("select * from med")
+        for i in cur_col:
+            n.append(i[1])
+        pharm_db.commit()
+
+        tk.Label(self, text="BILLING SYSTEM", bg="cyan4", fg="white", font="Arial 25").pack(fill=tk.BOTH, expand=1)
+
+        tk.Label(self, text='Customer Name: ').pack()
+        self.name = tk.Entry(self, bg=bg, fg=fg).pack()
+
+        self.protocol('WM_DELETE_WINDOW', self.close_bill)
+        tk.Button(self, width=15, text='Add Valued Customer', command=self.add_valued).pack()
+        tk.Button(self, width=15, text='Reset Bill', command=self.clear_bill).pack()
+        tk.Button(self, width=15, text='Print Bill', command=lambda: self.print_file(bill)).pack()
+        tk.Button(self, width=15, text='Save Bill', command=self.make_bill).pack()
+
+        tk.Label(self, text='Enter phone: ').pack()
+        self.phone = tk.Entry(self, bg=bg, fg=fg).pack()
+        # self.phone.bind('<Return>', self.check_valued)
+
+        tk.Label(self, text="Valued", bg="grey", fg="white").pack()
+
+        tk.Label(self, text='-' * 115).pack()
+        tk.Label(self, text='SELECT PRODUCT', width=25, relief='ridge').pack()
+        tk.Label(self, text=' RACK  QTY LEFT     COST          ', width=25, relief='ridge').pack()
+        tk.Label(self, text='QUANTITY', width=20, relief='ridge').pack()
+        qtys = tk.Entry(self).pack()
+
+# Clear all data in the Billing Form
+    def clear_bill(self):
+        # reset text file to prevent misprint
+        # clear content of data-entry widgets
+        # reset menu flags
+        self.master.confirm_task("Trash this Bill?")
+
+# Prepare a printable Bill text file
+    def make_bill(self):
+        global bill_id
+        if bill_id != 0:
+            bill_id += 1
+        else:
+            cur_col.execute("select bill_id from bills")
+            #if cur_col.fetchall():
+            #    bill_id =
+            #else:
+            #    bill_id = 0;
+        pass
+
+# Close the Billing Menu
+    def close_bill(self):
+        if self.bill_accounted() or self.close_window(self, "Reset this Bill?"):
+            self.clear_bill()
+        # if transaction billed, account it >> clear bill >> close menu
+        try:
+            self.destroy()
+        except:
+            pass
+
+# Check if the Bill is being made for a Valued Customer
+    def check_valued(self):
+        name = self.name.get()
+        phone = self.phone.get()
+
+        if not name:
+            self.name.configure(bg="red", fg="white")
+        elif not phone:
+            self.phone.configure(bg="red", fg="white")
+        else:
+            cur_log.execute("select * from cus where name='%s' and phone='%s' and valued='%s'"
+                            % (name, phone, True))
+            pharm_db.commit()
+            if cur_log.fetchone() is not None:
+                self.valued.configure(bg="green", fg="white")
+                return true
+            else:
+                self.valued.configure(bg="red", fg="white")
+        return false
+
+# Add the customer getting billed, as new Valued Customer
+    def add_valued(self):
+        name = self.name.get()
+        phone = self.phone.get()
+
+        if not name:
+            self.name.configure(bg="red", fg="white")
+        elif not phone:
+            self.phone.configure(bg="red", fg="white")
+        else:
+            cur_col.execute("select * from cus where phone='%s'")
+            if cur_col.fetchone() is not None:
+                for i in cur_col:
+                    if i[2]:
+                        if i[0] != name:
+                            self.phone.configure(bg="red", fg="white")
+                            self.msg.showerror("Phone number already registered under another name!")
+                            return False
+                        else:
+                            self.name.configure(bg="green", fg="white")
+                            self.phone.configure(bg="green", fg="white")
+                            self.msg.showerror("Already a valued customer!")
+                            return False
+                if self.master.confirm_task(self, "Make " + name + " a valued customer?"):
+                    cur_col.execute("update cus set valued=? where phone = (?,?,?)", (name, phone, value))
+            else:
+                if self.master.confirm_task(self, "Add " + name + " as a new valued customer?"):
+                    cur_col.execute("modify into cus values(?,?,?)", (name, phone, value))
+            pharm_db.commit()
+        return false
+
+
+""" BACKGROUND TRANSACTION HANDLERS """
+# These functions are active from outside the billing class.
+# Their function is to modify the cart variable as the user
+# selects or deselects items from other menus like search results.
+# It has nothing to do with how the bill is made
+
+
+# Mark items for next Bill
+def append_to_cart(self):
+    cart.append(self.search_results.cur_selection()[1])
+
+
+# Remove items from Bill
+def delete_from_cart(self):
+    cart.remove(self.search_results.cur_selection()[1])
+
+
+# Preparing Bill for transaction, performing calculations, storing in database etc
+def prepare_bill(self):
+    qty.append(qtys.get())
